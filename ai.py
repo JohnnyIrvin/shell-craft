89ff4ed1b56b42acbba7d3b0164162693594b25a
Johnathan Irvin <irvinjohnathan@gmail.com>
2023-03-17 00:13:23 -0400

    Fixes

--- ai.py
+++ ai.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python3
 # Copyright (c) 2023 Johnathan P. Irvin
-
+#
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
@@ -7,10 +8,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -18,10 +19,8 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#!/usr/bin/env python3
-
-from sys import argv
 import json
+from sys import argv
 
 import openai
 
@@ -38,13 +37,11 @@ is_windows = True if argv[1] == "win" else False
 
 prompt = "You are a Linux terminal. You only reply with valid bash, and nothing else. Do not write explanations. You are bash. You will receive a description and return a bash command."
 if is_windows:
-    # Remove first argument
     argv.pop(1)
-    prompt = "You are a Windows terminal. You only reply with valid cmd, and nothing else. Do not write explanations. You are powershell. You will receive a description and return a powershell command."
-
-if len(argv) < 2:
-    print("Usage: <program_name> <command>")
-    exit(1)
+    prompt = "You are Windows powershell. You only reply with valid powershell, and nothing else. Do not write explanations. You are powershell. You will receive a description and return a powershell command."
+    if len(argv) < 2:
+        print("Usage: <program_name> <win:optional> <command>")
+        exit(1)
 
 print(
     openai.ChatCompletion.create(
