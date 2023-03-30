12de0051cd1adb575988d658996f104095aca432
Johnathan Irvin <irvinjohnathan@gmail.com>
2023-03-30 08:11:29 -0400

    refactor: model argument.

---
+++ shell_craft/cli/models.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2023 Johnathan P. Irvin and contributors
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+from argparse import ArgumentParser
+
+
+def add_arguments(parser: ArgumentParser):
+    """
+    Adds '--model' argument to the parser. This argument is used to specify
+    openai's model to use. The default is 'gpt-3.5-turbo'. This argument is
+    optional. The models are provided by the OpenAI API but not all models
+    are available to all users. Nor are all models implemented in the
+    shell_craft library.
+
+    Args:
+        parser (ArgumentParser): The parser to add the argument to.
+    """    
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="gpt-3.5-turbo",
+        help="The input to prompt the API with.",
+    )
