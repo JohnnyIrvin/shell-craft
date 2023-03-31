b6024b1e45aa83e0293cc38c19aca83335e49278
Johnathan Irvin <irvinjohnathan@gmail.com>
2023-03-31 15:09:07 -0400

    feat: Dynamic prompt importing
    * Optional arguments.

--- shell_craft/cli/parser.py
+++ shell_craft/cli/parser.py
@@ -25,6 +25,86 @@ from os.path import dirname
 from sys import argv, stdin
 
 
+def _has_required_args(module: "ModuleType", args: Namespace) -> bool:
+    """
+    Checks if the module has the required arguments. If the module does not
+    have the ARGUMENTS attribute, then it is assumed that the module does not
+    require any arguments. If the module does have the ARGUMENTS attribute,
+    then it is assumed that the module requires arguments as long as the
+    ARGUMENTS attribute is a dictionary
+    
+    If the ARGUMENTS attribute is a dictionary, then the module will be checked
+    to see if it has all of the required arguments. If the module has all of the
+    required arguments, then the module will be checked to see if the values of
+    the required arguments match the values of the arguments passed to the parser.
+    
+    If the values of the required arguments match the values of the arguments
+    passed to the parser, are in a list of acceptable values, or pass a callable
+    function, then the module will be considered to have the required arguments.
+
+    Args:
+        module (ModuleType): The module to check for required arguments.
+        args (Namespace): The arguments passed to the parser.
+
+    Returns:
+        bool: True if the module has the required arguments, False otherwise.
+    """    
+    if not hasattr(module, "ARGUMENTS"):
+        return True
+    
+    if not isinstance(module.ARGUMENTS, dict):
+        return True
+    
+    for key in module.ARGUMENTS:
+        if key not in args:
+            return False
+        
+        arg_value = getattr(args, key)
+        mod_value = module.ARGUMENTS[key]
+
+        if isinstance(mod_value, str) and arg_value != mod_value:
+            return False
+
+        if isinstance(mod_value, list) and arg_value not in mod_value:
+            return False
+        
+        if callable(mod_value) and not mod_value(arg_value):
+            return False
+            
+    return True
+
+def _add_args_parser(parser: ArgumentParser, module: "ModuleType", args: Namespace):
+    """
+    Adds the given module's subparsers and arguments to the parser.
+    
+    Args:
+        parser (ArgumentParser): The parser to add the module to.
+        module (ModuleType): The module to check for required arguments.
+        args (Namespace): The arguments passed to the parser.
+    """
+    if hasattr(module, "add_parser"):
+        module.add_parser(parser)
+
+    if hasattr(module, "add_arguments"):
+        module.add_arguments(parser)
+
+def _add_optional_args_parser(parser: ArgumentParser, module: "ModuleType", args: Namespace):
+    """
+    Checks if the module's required arguments are present in the arguments
+    passed to the parser. The module will be checked to see if it has all
+    of the required arguments. If the module has all of the required
+    arguments, then the module will be added to the parser.
+
+    Args:
+        parser (ArgumentParser): The parser to add the module to.
+        module (ModuleType): The module to check for required arguments.
+        args (Namespace): The arguments passed to the parser.
+    """
+    if not _has_required_args(module, args):
+        return
+    
+    _add_args_parser(parser, module, args)
+
 def initialize_parser(parser: ArgumentParser) -> ArgumentParser:
     """ 
     Add arguments to the parser. Loops through all modules in this package and
@@ -49,21 +129,27 @@ def initialize_parser(parser: ArgumentParser) -> ArgumentParser:
         ArgumentParser: The parser with arguments added. Fluent interface.
     """
     PARENT_PACKAGE = __name__.rsplit(".", 1)[0]
-    for module in listdir(dirname(__file__)):
-        module = module.removesuffix(".py")
+    modules: list["ModuleType"] = [
+        import_module(f".{module.removesuffix('.py')}", package=PARENT_PACKAGE)
+        for module in listdir(dirname(__file__))
+        if module.endswith(".py") and
+        module not in ["main.py", "parser.py", "__init__.py"]
+    ]
 
-        if module in ["main", "parser", "__init__"]:
-            continue
-        
-        try:
-            import_module(f".{module}", package=PARENT_PACKAGE).add_arguments(parser)
-        except AttributeError:
-            pass
+    modules_without_requirements = [
+        module for module in modules if not hasattr(module, "ARGUMENTS")
+    ]
+    modules_with_requirements = [
+        module for module in modules if hasattr(module, "ARGUMENTS")
+    ]
 
-        try:
-            import_module(f".{module}", package=PARENT_PACKAGE).add_parser(parser)
-        except AttributeError:
-            pass
+    KNOWN_ARGS, _ = parser.parse_known_args()
+    for module in modules_without_requirements:
+        _add_args_parser(parser, module, KNOWN_ARGS)
+
+    KNOWN_ARGS, _ = parser.parse_known_args() # More arguments may have been added
+    for module in modules_with_requirements:
+        _add_optional_args_parser(parser, module, KNOWN_ARGS)
 
     return parser
 
@@ -88,10 +174,13 @@ def get_arguments(parser: ArgumentParser) -> Namespace:
 
     return parser.parse_args(arguments)
 
-PARSER = initialize_parser(ArgumentParser(
-    prog="shell-craft",
-    description="Generating shell commands and code using natural language models (OpenAI ChatGPT).",
-))
+PARSER = initialize_parser(
+    ArgumentParser(
+        prog="shell-craft",
+        description="Generating shell commands and code using natural language models (OpenAI ChatGPT).",
+        add_help=False
+    )
+)
 
 __all__ = [
     "get_arguments",
