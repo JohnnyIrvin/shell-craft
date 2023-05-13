3b08c3406af11d02dbd5ae9d81b5bb0d34a86070
Johnathan Irvin <irvinjohnathan@gmail.com>
2023-05-13 12:06:41 -0400

    feat(cli): Limited --model choices.
    * https://platform.openai.com/docs/models/model-endpoint-compatibility

--- shell_craft/cli/models.py
+++ shell_craft/cli/models.py
@@ -37,4 +37,12 @@ def add_arguments(parser: ArgumentParser):
         type=str,
         default="gpt-3.5-turbo",
         help="The input to prompt the API with.",
+        choices=[
+            "gpt-4",
+            "gpt-4-0314",
+            "gpt-4-32k",
+            "gpt-4-32k-0314",
+            "gpt-3.5-turbo",
+            "gpt-3.5-turbo-0301",
+        ]
     )
