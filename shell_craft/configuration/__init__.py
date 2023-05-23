b9bbe77582638edf5d76f686108396c22fe7a2ba
Johnathan Irvin <irvinjohnathan@gmail.com>
2023-05-23 16:45:47 -0400

    feat(configuration): Implemented json configuration implementation

--- shell_craft/configuration/__init__.py
+++ shell_craft/configuration/__init__.py
@@ -19,7 +19,9 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 from .configuration import Configuration
+from .json import JSONConfiguration
 
 __all__ = [
-    "Configuration"
+    "Configuration",
+    "JSONConfiguration",
 ]
