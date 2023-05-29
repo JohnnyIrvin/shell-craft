19911ec6dd3ad39587486fa3ea3f204a13255f25
Johnathan Irvin <irvinjohnathan@gmail.com>
2023-05-29 17:15:42 -0400

    feat(service): Wrote service protocol

---
+++ shell_craft/service/service.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2023 Johnathan P. Irvin
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+from typing import Protocol
+
+
+class Service(Protocol):
+    def query(self, message: str) -> list[str]:
+        """
+        Querys the foreign service with the given message and returns the
+        results.
+
+        Args:
+            message (str): The message to query the foreign service with.
+
+        Returns:
+            list[str]: The results of the query.
+        """        
+        ...
