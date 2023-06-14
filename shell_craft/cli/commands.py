f660d95abf114826888fa77373832d7dc590c49c
Johnathan Irvin <irvinjohnathan@gmail.com>
2023-06-13 21:03:29 -0400

    feat(models): Updated Models, GPT 3.5 16k

--- shell_craft/cli/commands.py
+++ shell_craft/cli/commands.py
@@ -82,11 +82,13 @@ _COMMANDS = [
         help='The OpenAI model to use.',
         choices=[
             "gpt-4",
-            "gpt-4-0314",
+            "gpt-4-0613",
             "gpt-4-32k",
-            "gpt-4-32k-0314",
+            "gpt-4-32k-0613",
             "gpt-3.5-turbo",
-            "gpt-3.5-turbo-0301",
+            "gpt-3.5-turbo-0613",
+            'gpt-3.5-turbo-16k',
+            'gpt-3.5-turbo-16k-0613',
         ]
     ),
     Command(
